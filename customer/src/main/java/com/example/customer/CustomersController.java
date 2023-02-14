8e1ab85a0987ed5f57febde3a120fbccf58b433f
suraj <suraj200981@yahoo.co.uk>
2023-02-14 20:32:37 +0100

    changing ports with applicaiton.yaml via spring-boot-starter dependancy

--- customer/src/main/java/com/example/customer/CustomersController.java
+++ customer/src/main/java/com/example/customer/CustomersController.java
@@ -1,8 +1,10 @@
 package com.example.customer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/")
 public class CustomersController {
