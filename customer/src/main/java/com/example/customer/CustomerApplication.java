fe315da37ce07ebdd65ad48afc87643719d393f8
suraj <suraj200981@yahoo.co.uk>
2023-02-28 19:08:10 +0100

    created first get request with JPA interface repo. I am also automatically inserting data on spring boot start up

--- customer/src/main/java/com/example/customer/CustomerApplication.java
+++ customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -1,27 +1,38 @@
 package com.example.customer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
-public class CustomerApplication {
+@RequestMapping("api/v1/customers")
+public class CustomerApplication implements CommandLineRunner {
+
+    //inject customer repository
+    private final CustomerRepository customerRepository;
+
+    public CustomerApplication(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class,args);
     }
-
-    @GetMapping("/")
-    public GreetResponse greet(){
-        return new GreetResponse("Hello World", new Person("Suraj", 24));
-    }
-    record GreetResponse(String greeting, Person person){
-    }
-
-    record Person (String name, Integer age){
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
 
-
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer1 = new Customer(1,"John", "Doe", "john.doe@example.com", 25);
+        customerRepository.save(customer1);
+    }
 }
